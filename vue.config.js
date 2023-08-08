db76dff5a01a1a608f65d33e7e2e5dc24624faa8
Mikolaj Hilgert <mikolaj.hilgert@protonmail.com>
2023-08-08 09:14:51 +0200

    add actions

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
+publicPath: process.env.NODE_ENV === "production" ? "/mikolajhilgerthub.io/" : "/",
 module.exports = defineConfig({
   transpileDependencies: true,
 
